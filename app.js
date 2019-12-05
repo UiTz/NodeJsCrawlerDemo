6e1170197b2d904cb5785adcddee7d1521169a75
ShiTou <shitou@ShiToudeMacBook-Pro.local>
2019-12-05 17:59:49 +0800

    首次提交少量可以爬取，大量数据爬取时处理失败

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+!function () {
+  const express = require('express');
+  const app = express();
+  // const cheerio = require('cheerio');
+  // const superagent = require('superagent');
+
+  app.url = 'https://cnodejs.org'
+
+  require('./routes/index')(app)
+
+  app.listen('8081', () => {
+    console.log('http://localhost:8081/')
+  })
+}()
